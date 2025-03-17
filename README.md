da23cd1e83db7da6b5217f4f96c84acc499a66a6
bogdann29 <bog_29@mail.ru>
2025-03-17 18:48:35 +0300

    add program options

--- README.md
+++ README.md
@@ -12,12 +12,18 @@ To build the project, follow these steps:
     cmake --build build
     ```
 
-3. Run tests
+3. Run tests:
     ```bash
     ctest --test-dir build
     ```
 
 4. Run the executable:
     ```bash
-    ./build/main
+    ./build/main --graph graph.txt --output result.txt
     ```
+
+    Options:
+    * --graph <filename> or -g <filename> — specifies the input file containing the graph description(default: gr.txt)
+    * --output <filename> or -o <filename> — specifies the output file (default: result.txt)
+    * --help or -h — displays the help message
+    
--- main.cpp
+++ main.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <boost/program_options.hpp>
 #include "src/helper.h"
 
+namespace po = boost::program_options;
 
-int main(){
-    std::string graph_fname = "gr.txt", output_fname = "result.txt";
+int main(int argc, char* argv[]){
+    std::string graph_fname, output_fname;
+
+    po::options_description desc("Allowed options");
+
+    desc.add_options()("help,h", "produce help message")
+                    ("graph,g", po::value<std::string>(&graph_fname)->default_value("gr.txt"), "file with graph description")
+                    ("output,o", po::value<std::string>(&output_fname)->default_value("result.txt"), "output file name");
+
+    po::variables_map varMap;
+    po::store(po::parse_command_line(argc, argv, desc), varMap);
+
+    try{
+        po::notify(varMap);
+    }
+    catch(std::exception& ex){
+        std::cout << ex.what() << std::endl;
+        return -1;
+    }
+
+    if (varMap.count("help") || varMap.count("h"))
+    {
+        std::cout << desc << "\n";
+        return 1;
+    }
 
     std::ifstream file(graph_fname);
     if (!file.is_open()) {
-        throw std::runtime_error("Не удалось открыть файл" + graph_fname);
+        std::cout << "Не удалось открыть файл " << graph_fname << std::endl;
+        return -1;
     }
 
     std::vector<std::vector<size_t>> graph;
